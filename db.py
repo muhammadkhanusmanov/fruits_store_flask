f0a0f68cfc75e1ea261b0a0ce25900e860bb1d91
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-02-25 22:29:26 -0600

    db

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import tinydb
 
 class GroceryDB:
     def __init__(self):
-        self.db = tinydb.TinyDB('fruitDB.json',indent=4, separators=(',', ': '))
+        self.db = tinydb.TinyDB('db.json',indent=4, separators=(',', ': '))
         self.table = self.db.table('grocery')
 
     def add(self, fruit: dict):
@@ -53,4 +53,4 @@ class GroceryDB:
         q = tinydb.Query()
         return self.table.search(q.price == price)
     
-    
+
